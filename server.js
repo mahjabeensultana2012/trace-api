ded5d66330eb1f7026177b1e5d49ae29b2622f40
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-24 07:34:10 -0600

    register info in login table

--- server.js
+++ server.js
@@ -72,18 +72,32 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
+  const hash = bcrypt.hashSync(password);
 
-  db('users')
-    .returning('*')
-    .insert({
-      name: name,
-      email: email,
-      joined: new Date(),
-    })
-    .then(user => {
-      res.json(user[0]);
-    })
-    .catch(err => res.status(400).json('Error! unable to register'));
+  db.transaction(trx => {
+    trx
+      .insert({
+        email: email,
+        hash: hash,
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date(),
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+      })
+      .catch(err => res.status(400).json('Error! unable to register'));
+  });
 });
 
 app.get('/profile/:id', (req, res) => {
